eca16c58f2f5b99a5ab6f73103462c7bca0f4b08
FelipeM09 <manuelfelipeme@ufps.edu.co>
2021-05-26 17:45:49 -0500

    Primer ejercicio SITPRE

---
+++ js/proceso.js
@@ -0,0 +1,13 @@
+async function leerJSON(url) {
+
+    try {
+      let response = await fetch(url);
+      let user = await response.json();
+      return user;
+    } catch(err) {
+      
+      alert(err);
+    }
+  }
+
+  
