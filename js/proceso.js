38b96d2ecc5cdd9a8cb8548cc269738130ca724e
FelipeM09 <manuelfelipeme@ufps.edu.co>
2021-05-27 08:06:13 -0500

    asdsa

--- js/proceso.js
+++ js/proceso.js
@@ -10,4 +10,35 @@ async function leerJSON(url) {
     }
   }
 
+  function mostrar(){
+    
+    var url = "https://raw.githubusercontent.com/FelipeM09/Notas_Sitpre/main/json/datos.json";
+        
+    leerJSON(url).then(datos => {
+        
+        console.log("hola");
+        drawTable();
   
+        // des.innerHTML = drawTableDescripcion(descripcion);
+        // resultado.innerHTML = drawTable(alumnos) ;
+    })
+  }
+
+  function drawTable() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Name');
+    data.addColumn('number', 'Salary');
+    data.addColumn('boolean', 'Full Time Employee');
+    data.addRows([
+      ['Mike',  {v: 10000, f: '$10,000'}, true],
+      ['Jim',   {v:8000,   f: '$8,000'},  false],
+      ['Alice', {v: 12500, f: '$12,500'}, true],
+      ['Bob',   {v: 7000,  f: '$7,000'},  true]
+    ]);
+
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+  }
+
+  mostrar();
